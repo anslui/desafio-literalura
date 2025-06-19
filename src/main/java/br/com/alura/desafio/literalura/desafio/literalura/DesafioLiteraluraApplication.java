6996064ff5dc52568a11c9546a01312bf68c1340
Anselmo Luiz <ansehelm2001@gmail.com>
2025-06-19 19:27:15 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/desafio/literalura/desafio/literalura/DesafioLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafio.literalura.desafio.literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/desafio/literalura/desafio/literalura/DesafioLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafio.literalura.desafio.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
