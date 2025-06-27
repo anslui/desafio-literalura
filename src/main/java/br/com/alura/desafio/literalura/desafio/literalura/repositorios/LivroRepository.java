e299a420220868066641f7631f60b2d788b054ce
Anselmo Luiz <ansehelm2001@gmail.com>
2025-06-27 16:58:54 -0300

    Added find Top 10 derived query.

--- src/main/java/br/com/alura/desafio/literalura/desafio/literalura/repositorios/LivroRepository.java
+++ src/main/java/br/com/alura/desafio/literalura/desafio/literalura/repositorios/LivroRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public interface LivroRepository extends JpaRepository<Livro, Long> {
     Optional<Livro> findByTituloIgnoreCase(String titulo);
     List<Optional<Livro>> findByIdiomaIgnoreCase(String idioma);
+    List<Livro> findTop10ByOrderByQuantidadeDownloadsDesc();
 
     @Query("SELECT DISTINCT l.idioma FROM Livro l")
     List<String> buscarIdiomasDisponiveis();
